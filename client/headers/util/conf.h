6915455f38d176ab7a9e8e13b6ad7e391a518a6c
juan dambra <juan.dambra@gmail.com>
2018-03-25 23:32:28 -0300

    Validacion de campos y getters de configuracion

--- client/headers/util/conf.h
+++ client/headers/util/conf.h
@@ -1,21 +1,35 @@
 #include <iostream>
 #include <yaml-cpp/yaml.h>
+#include "util/Constants.h"
 #ifndef CONF_H
 #define CONF_H
 
-
 using namespace std;
+
+enum valorInvalido {
+  VALOR_INVALIDO = 5
+};
+
+enum casacas {
+  PRINCIPAL = 1,
+  ALTERNATIVA = 2,
+};
 
 class Conf {
   public:
-    string formacion;
-    string casaca;    //principal true o alternativa false
-    string debugLevel;
 
+    string getFormacion();
+    int getCasaca();    //principal true o alternativa false
+    int getDebugLevel();
     int loadConf(string file);
+
     void printConf();
   private:
     YAML::Node config;
+    string formacion;
+    int casaca;    //principal true o alternativa false
+    int debugLevel;
+
 };
 
 #endif
--- client/src/util/conf.cpp
+++ client/src/util/conf.cpp
@@ -3,22 +3,63 @@
 
 using namespace std;
 
+int chooseDebugLevel(string str) {
+        if (!str.compare("debug")) {
+                return LOG_DEBUG;
+        }
+        else if (!str.compare("info")) {
+                return LOG_INFO;
+        }
+        else if (!str.compare("error")) {
+                return LOG_ERROR;
+        }
+        else
+                return VALOR_INVALIDO;
+}
+
+int chooseCasaca(string str) {
+        if (!str.compare("principal")) {
+                return LOG_DEBUG;
+        }
+        else if (!str.compare("alternativa")) {
+                return LOG_INFO;
+        }
+        else
+                return VALOR_INVALIDO;
+}
+
 int Conf::loadConf(string file){
-  try {
-    config = YAML::LoadFile(file);
-  }
-  catch (YAML::BadFile e) {
-   printf("bad file\n");
-   return 1;
-  }
-  debugLevel = config["debug"]["level"].as<std::string>();
-  formacion = config["equipo"]["formacion"].as<std::string>();
-  casaca = config["equipo"]["casaca"].as<std::string>();
-  return 0;
+        try {
+                config = YAML::LoadFile(file);
+        }
+        catch (YAML::BadFile e) {
+                printf("bad file\n");
+                return 1;
+        }
+        debugLevel = chooseDebugLevel(config["debug"]["level"].as<std::string>());
+        formacion = config["equipo"]["formacion"].as<std::string>();
+        casaca = chooseCasaca(config["equipo"]["casaca"].as<std::string>());
+        if((debugLevel == VALOR_INVALIDO)|| (casaca == VALOR_INVALIDO)) { //falta poner la validacion de la formacion
+                printf("bad conf\n");
+                return 1;
+        }
+        return 0;
+}
+
+int Conf::getDebugLevel() {
+        return debugLevel;
+}
+
+string Conf::getFormacion(){
+        return formacion;
+}
+
+int Conf::getCasaca() {
+        return casaca;
 }
 
 void Conf::printConf(){
-  cout << "Debug Level: " << debugLevel << "\n";
-  cout << "Formacion: "<< formacion << "\n";
-  cout << "Casaca: " <<  casaca << "\n";
+        cout << "Debug Level: " << getDebugLevel() << "\n";
+        cout << "Formacion: "<< getFormacion() << "\n";
+        cout << "Casaca: " <<  getCasaca() << "\n";
 }
