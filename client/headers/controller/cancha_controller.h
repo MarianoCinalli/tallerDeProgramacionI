f17d730a32ed0e3e9ceb62a2e761c8515dad76fb
Nicolas Cian <nicolas.cian@gmail.com>
2018-03-29 15:35:28 -0300

    Agrego cancha controller, muestro ventana segun tutorial SDL

---
+++ client/headers/controller/cancha_controller.h
@@ -0,0 +1,11 @@
+#ifndef CANCHA_CONTROLLER_H
+#define CANCHA_CONTROLLER_H
+
+#include <fstream>
+#include <stdio.h>
+
+using namespace std;
+
+int startView();
+
+#endif // CANCHA_CONTROLLER_H
--- client/src/main.cpp
+++ client/src/main.cpp
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include "util/Constants.h"
 #include "util/logger.h"
+#include "controller/cancha_controller.h"
 
 // Global variables ---------------------------------------
 int LOG_MIN_LEVEL = LOG_ERROR; // Cambiar por parametro parseado.
@@ -16,8 +17,13 @@ int main(int argc, char* argv[]) {
     // Log initialization ---------------------------------
     LOG_FILE_POINTER.open(logFileName, std::ofstream::app);
     logSessionStarted();
+    
     // Program
+    startView();
+
+
     logSessionFinished();
     LOG_FILE_POINTER.close();
+
     return 0;
 }
