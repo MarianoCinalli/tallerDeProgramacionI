4719af8a0c6cc1fd44d49adaf62793a1e33c8c10
marianocinalli <mariano.cinalli@hotmail.com>
2018-04-07 20:12:06 +0000

    Removing comment

--- client/headers/view/PitchView.h
+++ client/headers/view/PitchView.h
@@ -8,9 +8,7 @@ VistaCancha:
     - render() dibuja todo.
     Primero la cancha y despues a cada player view,
     les transforma las coordenadas para que queden el la
-    ventana (falta el metodo get coordinates y agregarle
-    una referencia al Player model dentro de PlayerSpriteManager)
-    y despues le hace el render() a todas los PlayerSpriteManager.
+    ventana y despues le hace el render() a todas los PlayerSpriteManager.
 */
 
 #ifndef PITCHVIEW_H
