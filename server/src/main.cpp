781f1b7f65e2b3ff60658caed006f3df08c8991e
marianocinalli <mariano.cinalli@hotmail.com>
2018-05-05 19:32:12 +0000

    Adding missing includes and function

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -12,14 +12,16 @@
 #include "controller/ActionsManager.h"
 #include "controller/GameController.h"
 #include "view/Camera.h"
-#include <stdio.h>
 
 #include <stdlib.h>
-#include <errno.h>
-#include <string.h>
-#include <netdb.h>
-#include <arpa/inet.h>
-#include <sys/types.h>
+// For server
+#include <sys/socket.h>
+#include <netinet/in.h>
+// For threads
+#include <pthread.h>
+
+#define NUM_THREADS 5
+#define PORT 8080
 
 
 // Global variables ---------------------------------------
@@ -33,126 +35,122 @@ std::string CLI_LOG_LEVEL = "";
 // Global variables ---------------------------------------
 
 
-void imprimir_ayuda()
-{
-	cout << "Usage:\n";
-	cout << "main -h\n";
-	cout << "main -V\n";
-	cout << "main [options]\n";
-	cout << "-V, --version      Imprimir version y salir.\n";
-	cout << "-h, --help     Imprimir esta ayuda.\n";
-	cout << "-i, --input        Path del archivo de configuracion YAML.\n";
-	cout << "-l, --logLevel        El nivel de log para la aplicacion. info;debug;error\n";
-	cout << "Ejemplo:\n";
-	cout << "main -i ~/conf.yaml -l info\n";
-	cout << "\nREFERENCIA\nCon las flechas: se corre, \nCon W sprint, \nCon A barrida, \nCon S: patear, \nCon BARRA: cambiar de jugador";
+void imprimir_ayuda() {
+    cout << "Usage:\n";
+    cout << "main -h\n";
+    cout << "main -V\n";
+    cout << "main [options]\n";
+    cout << "-V, --version      Imprimir version y salir.\n";
+    cout << "-h, --help     Imprimir esta ayuda.\n";
+    cout << "-i, --input        Path del archivo de configuracion YAML.\n";
+    cout << "-l, --logLevel        El nivel de log para la aplicacion. info;debug;error\n";
+    cout << "Ejemplo:\n";
+    cout << "main -i ~/conf.yaml -l info\n";
+    cout << "\nREFERENCIA\nCon las flechas: se corre, \nCon W sprint, \nCon A barrida, \nCon S: patear, \nCon BARRA: cambiar de jugador";
 }
 
-void imprimir_version()
-{
-	cout << "Version = tp1\n";
+void imprimir_version() {
+    cout << "Version = tp1\n";
 }
 
-int chequearOpciones(int argc, char* argv[])
-{
-	int ch;
+int chequearOpciones(int argc, char* argv[]) {
+    int ch;
 
-	std::string tempConf = "";
-	while (1) {
-		static struct option long_options[] = {
-			//Flags posibles
-			{ "version",  no_argument,	 0, 'v' },
-			{ "help",     no_argument,	 0, 'h' },
-			{ "input",    required_argument, 0, 'i' },
-			{ "logLevel", required_argument, 0, 'l' },
-			{ 0,	      0,		 0, 0	}
-		};
+    std::string tempConf = "";
+    while (1) {
+        static struct option long_options[] = {
+            //Flags posibles
+            { "version",  no_argument,   0, 'v' },
+            { "help",     no_argument,   0, 'h' },
+            { "input",    required_argument, 0, 'i' },
+            { "logLevel", required_argument, 0, 'l' },
+            { 0,          0,         0, 0   }
+        };
 
-		int option_index = 0;
+        int option_index = 0;
 
-		ch = getopt_long(argc, argv, "vhi:l:",
-				 long_options, &option_index);
+        ch = getopt_long(argc, argv, "vhi:l:",
+                         long_options, &option_index);
 
-		/* Detecta fin de opciones. */
-		if (ch == -1)
-			break;
+        /* Detecta fin de opciones. */
+        if (ch == -1)
+            break;
 
-		switch (ch) {
-		case 0:
-			if (long_options[option_index].flag != 0)
-				break;
-			printf("Opcion %s", long_options[option_index].name);
-			if (optarg) {
-				printf(" con argumentos %s", optarg);
-				printf("\n");
-				break;
-			}
+        switch (ch) {
+            case 0:
+                if (long_options[option_index].flag != 0)
+                    break;
+                printf("Opcion %s", long_options[option_index].name);
+                if (optarg) {
+                    printf(" con argumentos %s", optarg);
+                    printf("\n");
+                    break;
+                }
 
-		case 'v':
-			imprimir_version();
-			return 1;
-			break;
+            case 'v':
+                imprimir_version();
+                return 1;
+                break;
 
-		case 'h':
-			imprimir_ayuda();
-			return 1;
-			break;
+            case 'h':
+                imprimir_ayuda();
+                return 1;
+                break;
 
-		case 'i':
-			tempConf = optarg;
-			break;
+            case 'i':
+                tempConf = optarg;
+                break;
 
-		case 'l':
-			CLI_LOG_LEVEL = optarg;
-			break;
+            case 'l':
+                CLI_LOG_LEVEL = optarg;
+                break;
 
-		default:
-			printf("Opcion invalida\n\n");
-			return 1;
-		}
-	}
-	if (tempConf != "")
-		confFileName = tempConf;
-	else
-		confFileName = "conf.yaml";
-	return 0;
+            default:
+                printf("Opcion invalida\n\n");
+                return 1;
+        }
+    }
+    if (tempConf != "")
+        confFileName = tempConf;
+    else
+        confFileName = "conf.yaml";
+    return 0;
 }
 
 extern int errno;
 #define SERVER_PORT 4321
 
 
-int main(int argc, char* argv[])
-{
-	if (chequearOpciones(argc, argv)) {
-		//Si da 1 es o la version o el help o un flag inexistente.
-		printf("Salida del programa por flags o argumento invalido\n");
-		return SALIDA_LINEA_COMANDOS;
-	}
-	// Inicializacion -------------------------------------
-	LOG_FILE_POINTER.open(logFileName, std::ofstream::app);
-	logSessionStarted();
-	// Inicializacion -------------------------------------
-	log("Main: Cargando configuracion...", LOG_INFO);
-	Conf* configuration = new Conf(defaultConfFileName, defaultSpritesFileName);
-	configuration->loadConf(confFileName);
-	log("Main: Configuracion cargada: ", configuration, LOG_INFO);
-	int cliLogLevelInt = LOG_WRONGLEVEL;
-	LOG_MIN_LEVEL = configuration->getDebugLevel();
-	if (CLI_LOG_LEVEL != "") {
-		cliLogLevelInt = getLogLevelFromString(CLI_LOG_LEVEL);
-		if (cliLogLevelInt == LOG_WRONGLEVEL)
-			log("Main: Nivel de logeo especificado erroneo " + CLI_LOG_LEVEL + " usando default valor de config.", LOG_ERROR);
-		else
-			LOG_MIN_LEVEL = cliLogLevelInt;
-	}
-	log("Main: Nivel de log cambiado a: ", getMessageLevelString(LOG_MIN_LEVEL), LOG_ERROR);
-	delete(configuration);
-	log("Main: Juego inicializado correctamente.", LOG_INFO);
-	// Main loop ------------------------------------------
-	// Crear game manager.
-	// Crear broadcaster.
-	// Crear server.
+int main(int argc, char* argv[]) {
+    if (chequearOpciones(argc, argv)) {
+        //Si da 1 es o la version o el help o un flag inexistente.
+        printf("Salida del programa por flags o argumento invalido\n");
+        return SALIDA_LINEA_COMANDOS;
+    }
+    // Inicializacion -------------------------------------
+    LOG_FILE_POINTER.open(logFileName, std::ofstream::app);
+    logSessionStarted();
+    // Inicializacion -------------------------------------
+    log("Main: Cargando configuracion...", LOG_INFO);
+    Conf* configuration = new Conf(defaultConfFileName, defaultSpritesFileName);
+    configuration->loadConf(confFileName);
+    log("Main: Configuracion cargada: ", configuration, LOG_INFO);
+    int cliLogLevelInt = LOG_WRONGLEVEL;
+    LOG_MIN_LEVEL = configuration->getDebugLevel();
+    if (CLI_LOG_LEVEL != "") {
+        cliLogLevelInt = getLogLevelFromString(CLI_LOG_LEVEL);
+        if (cliLogLevelInt == LOG_WRONGLEVEL)
+            log("Main: Nivel de logeo especificado erroneo " + CLI_LOG_LEVEL + " usando default valor de config.", LOG_ERROR);
+        else
+            LOG_MIN_LEVEL = cliLogLevelInt;
+    }
+    log("Main: Nivel de log cambiado a: ", getMessageLevelString(LOG_MIN_LEVEL), LOG_ERROR);
+    delete(configuration);
+    log("Main: Juego inicializado correctamente.", LOG_INFO);
+    // Main loop ------------------------------------------
+    // Crear game manager.
+    // Crear broadcaster.
+    // Crear server.
 
     int server_fd, new_socket;
     struct sockaddr_in address;
@@ -218,7 +216,7 @@ int main(int argc, char* argv[])
         log("In main: socket %d closed\n", sockets[index], LOG_INFO);
     }
 
-	logSessionFinished();
-	LOG_FILE_POINTER.close();
-	return 0;
+    logSessionFinished();
+    LOG_FILE_POINTER.close();
+    return 0;
 }
