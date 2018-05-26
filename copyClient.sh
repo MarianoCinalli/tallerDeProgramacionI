aeb02862e931e2d7efae5caecd66bbb70b0ce93c
marianocinalli <mariano_cinalli@hotmail.com>
2018-05-25 22:32:05 -0300

    Allowing to delete copies

--- copyClient.sh
+++ copyClient.sh
@@ -8,11 +8,46 @@ function printHelp {
     " >&2
 }
 
-showHelpFlag=
+function createCopies {
+    # Validacion
+    is_a_number_regex='^[0-9]$'
+    if ! [[ $1 =~ $is_a_number_regex ]] ; then
+       echo "Error! La cantidad no es un numero." >&2
+       exit 1
+    fi
 
-while getopts 'h' flag; do
+    echo "Creando $1 directorio/s..."
+
+    i=0
+    while [ $i -lt $1 ]; do
+        new_client_dir="copy"$i"OfClient"
+        # Si existia, limpiar lo de adentro.
+        rm -fr "$new_client_dir/"
+        # Crear los directorios.
+        echo "Creando: $new_client_dir..."
+        mkdir -p $new_client_dir
+        # Copiar archivos.
+        cp client/main "$new_client_dir/"
+        cp -R client/config "$new_client_dir/"
+        i=$[$i+1]
+    done
+
+    echo "Directorio/s creados."
+}
+
+function deleteCopies {
+    echo "Borrando copias..."
+    rm -fr copy*OfClient
+    echo "Copias borradas."
+}
+
+showHelpFlag=
+deleteFlag=
+
+while getopts 'hd' flag; do
     case "${flag}" in
         h)    showHelpFlag=1 ;;
+        d)    deleteFlag=1 ;;
         *)    echo "Unexpected option ${flag}" ;;
     esac
 done
@@ -22,37 +57,14 @@ if [ ! -z "$showHelpFlag" ]; then
     exit 0
 fi
 
-
-
-if [ ! -z $1 ]; then
-    client_quantity=$1
+if [ ! -z "$deleteFlag" ]; then
+    deleteCopies
 else
     client_quantity=1
+    if [ ! -z $1 ]; then
+        client_quantity=$1
+    fi
+    createCopies $client_quantity
 fi
-
-# Validacion
-is_a_number_regex='^[0-9]$'
-if ! [[ $client_quantity =~ $is_a_number_regex ]] ; then
-   echo "Error! La cantidad no es un numero." >&2
-   exit 1
-fi
-
-echo "Creando $client_quantity directorio/s..."
-
-i=0
-while [ $i -lt $client_quantity ]; do
-    new_client_dir="copy"$i"OfClient"
-    # Si existia, limpiar lo de adentro.
-    rm -fr "$new_client_dir/"
-    # Crear los directorios.
-    echo "Creando: $new_client_dir..."
-    mkdir -p $new_client_dir
-    # Copiar archivos.
-    cp client/main "$new_client_dir/"
-    cp -R client/config "$new_client_dir/"
-    i=$[$i+1]
-done
-
-echo "Directorio/s creados."
 
 exit 0
