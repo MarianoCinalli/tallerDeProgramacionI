6c8ba5874a43ca35b481e8edaa5cc0d2f2e2b89b
marianocinalli <mariano_cinalli@hotmail.com>
2018-06-17 20:12:38 -0300

    Adding player areas, depending on formation

---
+++ server/headers/controller/PlayerAreas.h
@@ -0,0 +1,21 @@
+#include "util/Constants.h"
+#include "util/logger.h"
+#include "util/Rectangle.h"
+#include "controller/Areas.h"
+#include "controller/Areas312.h"
+#include "controller/Areas321.h"
+#include "controller/Areas33.h"
+
+#ifndef PLAYERAREAS_H
+#define PLAYERAREAS_H
+
+class PlayerAreas {
+    private:
+        Areas* areas;
+    public:
+        PlayerAreas();
+        PlayerAreas(int formation);
+        Rectangle* getForPlayer(int playerNumber);
+        ~PlayerAreas();
+};
+#endif // PLAYERAREAS_H
---
+++ server/src/controller/PlayerAreas.cpp
@@ -0,0 +1,33 @@
+#include "controller/PlayerAreas.h"
+
+PlayerAreas::PlayerAreas() {
+    log("PlayerAreas: Inicializando las areas...", LOG_INFO);
+    log("PlayerAreas: Formacion no especificada. Creando areas default.", LOG_DEBUG);
+    this->areas = new Areas();
+}
+
+PlayerAreas::PlayerAreas(int formation) {
+    log("PlayerAreas: Inicializando las areas...", LOG_INFO);
+    if (formation == 312) {
+        log("PlayerAreas: Formacion 312. Creando areas...", LOG_DEBUG);
+        this->areas = new Areas312();
+    } else if (formation == 321) {
+        log("PlayerAreas: Formacion 321. Creando areas...", LOG_DEBUG);
+        this->areas = new Areas321();
+    } else if (formation == 33) {
+        log("PlayerAreas: Formacion 33. Creando areas...", LOG_DEBUG);
+        this->areas = new Areas33();
+    } else {
+        log("PlayerAreas: No se pudo crear areas. Formacion desconocida: ", formation, LOG_ERROR);
+        this->areas = new Areas();
+    }
+}
+
+Rectangle* PlayerAreas::getForPlayer(int playerNumber) {
+    return this->areas->get(playerNumber);
+}
+
+PlayerAreas::~PlayerAreas() {
+    log("PlayerAreas: Liberando memoria. Borranda areas...", LOG_INFO);
+    delete(this->areas);
+}
