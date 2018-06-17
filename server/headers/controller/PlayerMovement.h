191d5345e30340d88664303e40d50a23bc333ed8
marianocinalli <mariano_cinalli@hotmail.com>
2018-06-17 20:12:55 -0300

    Using player areas and rectangles

--- server/headers/controller/PlayerMovement.h
+++ server/headers/controller/PlayerMovement.h
@@ -1,11 +1,12 @@
+#include "controller/PlayerAreas.h"
+#include "util/Rectangle.h"
+
 #ifndef PLAYERMOVEMENT_H
 #define PLAYERMOVEMENT_H
 
 class PlayerMovement {
     private:
-        int defenders;
-        int midfielders;
-        int attackers;
+        PlayerAreas* playerAreas;
     public:
         PlayerMovement();
         void setFormation(int formation);
--- server/src/controller/PlayerMovement.cpp
+++ server/src/controller/PlayerMovement.cpp
@@ -1,32 +1,21 @@
 #include "controller/PlayerMovement.h"
 
 PlayerMovement::PlayerMovement() {
-    this->defenders = 0;
-    this->midfielders = 0;
-    this->attackers = 0;
+    this->playerAreas = new PlayerAreas();
 }
 
 void PlayerMovement::setFormation(int formation) {
-    this->defenders = (formation / 100) % 10;
-    this->midfielders = (formation / 10) % 10;
-    this->attackers = formation % 10;
+    this->playerAreas = new PlayerAreas(formation);
 }
 
 bool PlayerMovement::canMoveTo(int x, int y, int playerNumber, bool isSelected) {
     bool canMove = false;
     if (!isSelected) {
-        /*
-        Rectangle* rectangle = this->getAreaForPlayer(playerNumber);
+        Rectangle* rectangle = this->playerAreas->getForPlayer(playerNumber);
         canMove = rectangle->isInside(x, y);
-        */
     }
     return canMove;
 }
-/*
-Rectangle* PlayerMovement::getAreaForPlayer(int playerNumber) {
-}
-*/
+
 PlayerMovement::~PlayerMovement() {
 }
-
-
