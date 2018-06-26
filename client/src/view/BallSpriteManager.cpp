a6aab35860db42b43b8d9b722766f6b27f1a9972
juan dambra <juan.dambra@gmail.com>
2018-06-25 22:14:31 -0300

    fix comments

--- client/src/view/BallSpriteManager.cpp
+++ client/src/view/BallSpriteManager.cpp
@@ -67,37 +67,6 @@ void BallSpriteManager::setMovingBallSprite() {
     }
     int height = this->ball->getHeight();
     int level = 0;
-    // if (height == 0) {
-    //     int orientation = (this->ball->getOrientation());
-    //     int orientationOffset = 0;
-    //     if (this->dibujoConOffset) {      //Para que dibuje una con offset y una no
-    //         switch (orientation) {
-    //             case PLAYER_ORIENTATION_UP:
-    //                 orientationOffset = 1;
-    //                 break;
-    //             case PLAYER_ORIENTATION_RIGHT:
-    //                 orientationOffset = 2;
-    //                 break;
-    //             case PLAYER_ORIENTATION_DOWN:
-    //                 orientationOffset = 3;
-    //                 break;
-    //             case PLAYER_ORIENTATION_LEFT:
-    //                 orientationOffset = 4;
-    //                 break;
-    //         }
-    //     }
-    //     if ((rollingCount % ROLLING_DIVISOR) == 0) {
-    //         log("BallSpriteManager: Creando el sprite del balon.", LOG_SPAM);
-    //         if ((this->sprite.x == 3 * SPRITE_SIZE) || (this->sprite.y != ( 21 + orientationOffset) * SPRITE_SIZE)) {
-    //             this->sprite.x = 0; // Reinicio la secuencia.
-    //             this->sprite.y = ((21 + orientationOffset) * SPRITE_SIZE);
-    //             this->cambioOffset();
-    //         } else {
-    //             this->sprite.x += SPRITE_SIZE; // Avanzo la secuencia en un frame.
-    //         }
-    //     }
-    // }
-    // else {
         if ((rollingCount % ROLLING_DIVISOR) == 0) {
             log("BallSpriteManager: Creando el sprite del balon aereo.", LOG_SPAM);
             level = (height / BALL_DECELERATE_CONST);
@@ -108,7 +77,6 @@ void BallSpriteManager::setMovingBallSprite() {
             else {
                 this->sprite.x += SPRITE_SIZE; // Avanzo la secuencia en un frame.
             }
-        // }
     }
     rollingCount ++;
 }
